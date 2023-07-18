324771b9de94c11ece9a276ba712fe70150e78f0
MattDB101 <matt@invi.so>
2023-07-18 14:12:58 +0100

    remove old

--- src/main/java/com/cyberskills/mastersbuilder/EndUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cyberskills.mastersbuilder;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EndUserRepository extends JpaRepository<EndUser, Long> {
-    
-}
