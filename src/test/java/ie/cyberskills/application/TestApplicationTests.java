8129b3668d2a0648fda65c02e3649e5f7df8883d
Maksims Kazoha <maksims.kazoha@mycit.ie>
2023-07-21 12:00:15 +0100

    renamed TestApllication.java to MainApplication.java and added my localhost and password for my local postgresql

--- src/test/java/ie/cyberskills/application/TestApplicationTests.java
+++ src/test/java/ie/cyberskills/application/TestApplicationTests.java
@@ -3,6 +3,7 @@ package ie.cyberskills.application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class TestApplicationTests {
 
